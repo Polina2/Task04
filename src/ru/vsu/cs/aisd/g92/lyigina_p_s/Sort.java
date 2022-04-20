32d2420a251943a52a6d821d3cc1d62eef303241
Haku <polina231003@mail.ru>
2022-04-20 14:53:29 +0300

    Quick sort fixed

--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/Sort.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/Sort.java
@@ -2,10 +2,12 @@ package ru.vsu.cs.aisd.g92.lyigina_p_s;
 
 public class Sort {
     public static <T extends Comparable<T>> void sort(T[] data) {
-        introSort(data, 0, data.length-1, Math.log((double) data.length)/Math.log(2.0));
+        introSort(data, 0, data.length-1, Math.log(data.length)/Math.log(2));
     }
 
     private static <T extends Comparable<T>> void introSort(T[] data, int start, int end, double maxDepth) {
+        if (start >= end)
+            return;
         if (end - start < 15) {
             insertionSort(data, start, end);
         } else if (maxDepth <= 0) {
@@ -32,21 +34,23 @@ public class Sort {
             } else
                 p = a;
             pivot = data[p];
+
             swap(data, p, end);
             int l = start;
             int r = end - 1;
-            while (l < r) {
-                while (data[l].compareTo(pivot) < 0) {
+            while (l <= r) {
+                while (l <= end-1 && data[l].compareTo(pivot) < 0) {
                     l++;
                 }
-                while (data[r].compareTo(pivot) >= 0) {
+                while (r >= start && data[r].compareTo(pivot) >= 0) {
                     r--;
                 }
                 if (l < r)
                     swap(data, l, r);
             }
             swap(data, l, end);
-            introSort(data, start, r, maxDepth-1);
+
+            introSort(data, start, l-1, maxDepth-1);
             introSort(data, l+1, end, maxDepth-1);
         }
     }
