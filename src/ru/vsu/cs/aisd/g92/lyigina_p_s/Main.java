abdf354c4ee70c4649803fa8245c7819c370312c
Haku <polina231003@mail.ru>
2022-04-20 20:30:18 +0300

    Heap sort and insertion sort done

--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/Main.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/Main.java
@@ -3,6 +3,9 @@ package ru.vsu.cs.aisd.g92.lyigina_p_s;
 public class Main {
 
     public static void main(String[] args) {
-
+        Integer[] arr = {62, 89, 98, 7, 90, 1, 29, 27, 24, 75, 39, 62, 61, 9, 1, 56, 75, 45, 22, 4, 72, 62, 75, 13, 3, 92, 88, 92, 11, 3, 45, 21, 23, 81, 29, 71, 3, 2, 32, 59, 81, 66, 41, 64, 29, 88, 6, 41, 60, 13};
+        Sort.sort(arr);
+        for (Integer i : arr)
+            System.out.print(i + " ");
     }
 }
--- src/ru/vsu/cs/aisd/g92/lyigina_p_s/Sort.java
+++ src/ru/vsu/cs/aisd/g92/lyigina_p_s/Sort.java
@@ -2,37 +2,53 @@ package ru.vsu.cs.aisd.g92.lyigina_p_s;
 
 public class Sort {
     public static <T extends Comparable<T>> void sort(T[] data) {
-        introSort(data, 0, data.length-1, Math.log(data.length)/Math.log(2));
+        introSort(data, 0, data.length-1, Math.log(data.length)/Math.log(2)-4);
     }
 
     private static <T extends Comparable<T>> void introSort(T[] data, int start, int end, double maxDepth) {
         if (start >= end)
             return;
         if (end - start < 15) {
+
+            for (int i = start; i <= end; i++)
+                System.out.print(data[i] + " ");
+            System.out.println("insertion sort ->");
+
             insertionSort(data, start, end);
+
+            for (int i = start; i <= end; i++)
+                System.out.print(data[i] + " ");
+            System.out.println();
         } else if (maxDepth <= 0) {
+
+            for (int i = start; i <= end; i++)
+                System.out.print(data[i] + " ");
+            System.out.println("heap sort ->");
+
             heapSort(data, start, end);
+
+            for (int i = start; i <= end; i++)
+                System.out.print(data[i] + " ");
+            System.out.println();
         } else {
             T pivot;
             int p;
-            int a = start;
-            int b = (start + end) / 2;
-            int c = end;
-            if (data[a].compareTo(data[b]) > 0) {
-                if (data[a].compareTo(data[c]) > 0) {
-                    if (data[b].compareTo(data[c]) > 0)
-                        p = b;
+            int middle = (start + end) / 2;
+            if (data[start].compareTo(data[middle]) > 0) {
+                if (data[start].compareTo(data[end]) > 0) {
+                    if (data[middle].compareTo(data[end]) > 0)
+                        p = middle;
                     else
-                        p = c;
+                        p = end;
                 } else
-                    p = a;
-            } else if (data[a].compareTo(data[c]) < 0) {
-                if (data[b].compareTo(data[c]) > 0)
-                    p = b;
+                    p = start;
+            } else if (data[start].compareTo(data[end]) < 0) {
+                if (data[middle].compareTo(data[end]) > 0)
+                    p = middle;
                 else
-                    p = c;
+                    p = end;
             } else
-                p = a;
+                p = start;
             pivot = data[p];
 
             swap(data, p, end);
@@ -56,11 +72,40 @@ public class Sort {
     }
 
     private static <T extends Comparable<T>> void heapSort(T[] data, int start, int end) {
+        for (int i = (end+start-1)/2; i > start; i--)
+            sift(data, i, start, end);
+        for (int i = end; i > start; i--) {
+            sift(data, start, start, i);
+            swap(data, start, i);
+        }
+    }
 
+    private static <T extends Comparable<T>> void sift(T[] data, int k, int start, int end) {
+        T value = data[k];
+        while (true) {
+            int childInd = 2*(k-start)+1+start;
+            if (childInd > end)
+                break;
+            if (childInd+1 <= end && data[childInd+1].compareTo(data[childInd])>0)
+                childInd++;
+            if (data[childInd].compareTo(value) < 0)
+                break;
+            data[k] = data[childInd];
+            k = childInd;
+        }
+        data[k] = value;
     }
 
     private static <T extends Comparable<T>> void insertionSort(T[] data, int start, int end) {
-
+        int i = start+1;
+        while (i <= end) {
+            int j = i;
+            while (j > start && data[j].compareTo(data[j-1]) < 0) {
+                swap(data, j, j - 1);
+                j--;
+            }
+            i++;
+        }
     }
 
     private static <T> void swap(T[] data, int a, int b) {
